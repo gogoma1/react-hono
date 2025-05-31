8bf8d9e030e722fdd628860523e085f1d9ec1003
gogoma1 <nicewui@gmail.com>
2025-05-31 19:17:52 +0900

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
+  server: {
+  proxy: {
+    // Hono API 경로들을 Wrangler 로컬 프록시로 전달
+    '/user': 'http://127.0.0.1:8787',
+    '/signin': 'http://127.0.0.1:8787',
+    '/example': 'http://127.0.0.1:8787',
+    // 또는 공통 접두사 '/api'를 사용했다면
+    // '/api': {
+    //   target: 'http://127.0.0.1:8787',
+    //   changeOrigin: true,
+    // }
+  }
+}
 });
